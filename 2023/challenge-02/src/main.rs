03b50a08dfce32e611f8b8ccf3d3a7ae3629462d
sneakycrow <zach@sneakycrow.dev>
2023-12-02 05:33:50 -0800

    add basic setup

--- 2023/challenge-02/src/main.rs
+++ 2023/challenge-02/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let filepath = std::env::args().nth(1).expect("No file provided");
+    // Store the largest value of cube colors: red, green, and blue
+    if let Ok(lines) = shared::read_lines(filepath) {
+        // Check if the cube number for each relative color is larger than the current largest
+        // If the any value is larger than the maximum value, skip the entire line
+    }
 }
