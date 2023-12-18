d701d26e14da70e8a141b43acafb158da6bacfb4
sneakycrow <zach@sneakycrow.dev>
2023-12-18 03:02:38 -0800

    add TODO's

--- 2023/challenge-14/src/main.rs
+++ 2023/challenge-14/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // TODO: Read in input
+    // TODO: Store all lines in single array
+    // TODO: For each string in array, check if its a
+    //  - O for a round rock
+    //  - # for a square rock
+    //  - . for an empty space
+    // First, check if the current index is a round rock, if so move forward with moving it
+    // Depending on the direction, check if the directional space is empty, if so move there (replace . with O)
+    // A space is not considered empty unless it's a .
+    // After all O's have been moved, check the distance from each O in a line to the last index and sum for the total distance (load)
+    // At the end, sum all loads to get the total load
 }
